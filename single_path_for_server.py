6d75cb1526aeed7f784efedee42b94817b71bbff
Kurinov Yuriy <kurinovurij@gmail.com>
2026-01-24 19:01:37 +0300

    server_script_fix

--- single_path_for_server.py
+++ single_path_for_server.py
@@ -13,8 +13,7 @@ save_config_copy(exp_id)
 
 theta, y, t = sparse_mc(p0, Lambda, lam, T, get_y_uniform, y_intervals)
 
-dxi = make_discretized_xi(t_net_filtering, g, sigma, theta, y, t)
-deta = make_discretized_eta(t_net_filtering, h, theta, y, t)
+observations = get_obs(t_net_filtering, theta, y, t).squeeze()
 
 
 filter = Filter(
@@ -31,8 +30,8 @@ y_est = [est[1]]
 
 start = time()
 
-for i, obs in enumerate(np.stack([dxi[1:], deta[1:]], axis=-1), start=1):
-    filter.update(obs)
+for i, obs in enumerate(observations, start=1):
+    filter.update(obs.squeeze())
     est = filter.estimate()
     if np.any(np.isnan(est[0])) or np.any(np.isnan(est[1])):
         print(f'nan on {i}-th iter')
@@ -52,4 +51,4 @@ print(f"exp_id: {exp_id}")
 theta_est = np.array(theta_est)
 y_est = np.array(y_est)
 
-save_path(exp_id, theta, y, t, theta_est, y_est, dxi, deta)
+save_path(exp_id, theta, y, t, theta_est, y_est, observations)
